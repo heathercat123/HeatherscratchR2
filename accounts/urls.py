5c8cfc0e79d5e2c9b756bd165bd0160ae05fb77d
Heathercat123 <antoineaurelief@icloud.com>
2024-11-30 12:19:38 -0500

    Added stub for log out link so the mobile forums while logged in doesn't error out

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^modal-registration/$', views.modal_registration, name="modal-registration"),
 	url(r'^check_username/(?P<username>.+)/$', views.check_username, name="check_username"),
+	url(r'^logout/$', views.modal_registration, name="logout"), # stub
 ]
