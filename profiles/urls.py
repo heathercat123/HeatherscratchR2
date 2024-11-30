9741ce1c74c99d43115b2d36718b77bd39ccbd01
Heathercat123 <antoineaurelief@icloud.com>
2024-11-30 14:25:58 -0500

    Change profile reversematch for forums

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-	url(r'^(?P<username>.+)/$', views.detail, name="profile"),
+	url(r'^(?P<username>.+)/$', views.detail, name="profile_detail"),
 ]
