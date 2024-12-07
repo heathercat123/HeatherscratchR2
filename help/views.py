b13ee79eeaff419e19f442ae8986aecba32a89ed
Heathercat123 <antoineaurelief@icloud.com>
2024-12-07 18:52:09 -0500

    Added some class static files and did a bit of tipping

--- help/views.py
+++ help/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader, Context
+from django.http import Http404
+from django.template import TemplateDoesNotExist
 
 # Create your views here.
 def tips(request, tip):
 	if tip[-1] == '/':
 		tip = tip[:-1]
-	return render(request, 'help/tips/' + tip + '.html')
+	try :
+		return render(request, 'help/tips/' + tip + '.html')
+	except TemplateDoesNotExist:
+           raise Http404("Tip does not exist")
